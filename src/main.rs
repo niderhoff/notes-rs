a38d4676f7515579c965dcb7c821a6582f690efe
Nicolas Iderhoff <hello@niid.xyz>
2022-01-27 15:18:46 +0100

    update error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-#![allow(unused)]
-
 use std::{
-    fs::{self, File, OpenOptions},
-    io::{BufRead, BufReader, Error, ErrorKind},
+    fs::{File, OpenOptions},
+    io::Write,
 };
 
 use clap::{App, AppSettings, Arg, ArgMatches};
@@ -43,55 +41,61 @@ impl Database {
             notes: Some(vec![]),
         }
     }
-    fn convert_to_note(line: &str) -> Note {
-        let mut split = line.split("|");
-        let id = split.nth(0).unwrap().parse::<usize>().unwrap();
-        let text = split.nth(1).unwrap().to_owned();
+    fn create_new_note(&self, line: &str) -> Note {
+        let id = self.notes.as_ref().map_or_else(|| 0, |x| x.len());
+        let text = line.to_owned();
         Note { id, text }
     }
-    fn replace(&mut self, note: Note) -> Result<(), Error> {
+    fn replace(&mut self, note: Note) -> Result<(), std::io::Error> {
         if let Some(notes) = &mut self.notes {
             let index = 1usize;
-            std::mem::replace(&mut notes[index], note);
+            let _ = std::mem::replace(&mut notes[index], note);
         }
+        // todo return old value
         Ok(())
     }
-    fn read_backup(&mut self) -> Result<usize, Error> {
-        let file = File::open(&self.path)?;
+    fn read_backup(&mut self) -> Result<usize, std::io::Error> {
         let buf = std::fs::read_to_string(&self.path)?;
         let mut notes = vec![];
         for line in buf.split("\n") {
-            let note = Database::convert_to_note(line);
+            let note = self.create_new_note(line);
             notes.push(note);
         }
         let len = notes.len();
         self.notes = Some(notes);
         Ok(len)
     }
-    fn write_backup(&self) -> Result<(), Error> {
-        if self.notes.is_some() {
-            let notes = self.notes.as_ref().unwrap();
-            let file = File::open(&self.path)?;
-            let m = notes.iter().map(|x| &x.text);
+    fn write_backup(&mut self) -> Result<(), std::io::Error> {
+        if let Some(notes) = self.notes.as_ref() {
+            let mut f: File = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open("notes.txt")?;
+            for note in notes {
+                writeln!(f, "{}|{}", note.id, note.text)?
+            }
         }
         Ok(())
     }
     /// add new note, returns the Note if finished
-    fn add(&mut self, buf: &str) -> Result<Note, Error> {
+    fn add(&mut self, buf: &str) -> Result<(), std::io::Error> {
         let id = self.read_backup()?;
         let n = Note {
             text: buf.to_owned(),
             id,
         };
         match &mut self.notes {
+            // if we have notes. push new note
             Some(notes) => {
-                notes.push(n.clone());
+                notes.push(n);
             }
+            // else create note vector with note in it.
             None => {
-                let notes = vec![n.clone()];
+                self.notes = Some(vec![n]);
             }
         };
-        Ok(n)
+        self.write_backup()
     }
     fn get_index_by_id(&self, id: &usize) -> Option<usize> {
         self.notes.as_ref()?.iter().position(|x| x.id == *id)
@@ -100,19 +104,24 @@ impl Database {
     fn get_note_by_id(&self, id: &usize) -> Option<&Note> {
         self.notes.as_ref()?.iter().find(|x| x.id == *id)
     }
-    fn delete(&mut self, id: &usize) -> Option<Note> {
-        let idx = self.notes.as_ref()?.iter().position(|x| x.id == *id)?;
-        let ns = &mut self.notes;
-        match ns {
-            Some(ns) => Some(ns.remove(idx)),
-            None => None,
-        }
+    fn delete(&mut self, id: &usize) -> Result<(), Box<dyn std::error::Error>> {
+        let _r = {
+            let idx = self.get_index_by_id(id).unwrap();
+            let ns = &mut self.notes;
+            match ns {
+                Some(ns) => Some(ns.remove(idx)),
+                None => None,
+            }
+        };
+        self.write_backup()?;
+        //Todo: return deleted note
+        Ok(())
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut db = Database::new(String::from("notes.txt"));
-    let mut app = App::new("notes")
+    let app = App::new("notes")
         .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(
             App::new("add")
@@ -139,13 +148,15 @@ fn main() {
                 .unwrap()
                 .collect::<Vec<&str>>()
                 .join(" ");
-            db.add(&buf);
+            let r = db.add(&buf);
             println!("added new note: {:?}", buf);
+            Ok(r?)
         }
         Some(("get", sub_m)) => match sub_m.value_of("id") {
             Some(id) => {
                 let id = id.parse::<usize>().unwrap();
                 db.get_note_by_id(&id).map(|x| println!("{:?}", x.text));
+                Ok(())
             }
             None => {
                 if let Some(notes) = db.notes {
@@ -153,11 +164,16 @@ fn main() {
                         println!("{}: {}", note.id, note.text);
                     }
                 }
+                Ok(())
             }
         },
         Some(("delete", sub_m)) => {
-            let got = db.delete(&Note::get_id(sub_m)).unwrap();
-            println!("removed note {}: {}", got.id, got.text);
+            let id = &Note::get_id(sub_m);
+            let _got = db.delete(id).unwrap();
+            println!("removed note {}", id);
+            //todo: use returned note
+            //println!("removed note {}: {}", got.id, got.text);
+            Ok(())
         }
         Some(("update", sub_m)) => {
             let id = Note::get_id(sub_m);
@@ -166,12 +182,13 @@ fn main() {
                 .unwrap()
                 .collect::<Vec<&str>>()
                 .join(" ");
-            db.replace(Note {
+            let r = db.replace(Note {
                 id,
                 text: buf.clone(),
             });
             println!("updated note {}: {}", id, &buf);
+            Ok(r?)
         }
-        _ => (),
+        _ => Ok(()),
     }
 }
